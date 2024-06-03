d429c517fd220bc0b2be2b60aadcddf0aa5e15ea
Lakshay Bhushan <thegreenfoxyit@gmail.com>
2024-06-03 16:17:20 +0530

    added workers ai and sanitized html

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import { Hono } from 'hono'
+import { zValidator } from "@hono/zod-validator"
+import { Hono } from "hono"
+import { z } from "zod"
+import sanitizeHtml from "sanitize-html"
 
 type Bindings = {
   [key in keyof CloudflareBindings]: CloudflareBindings[key]
@@ -6,8 +9,42 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>()
 
-app.get('/', (c) => {
-  return c.text('Hello this is backend!!!')
-})
+const isValidURL = (value: string) => {
+  try {
+    new URL(value)
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
+app.get(
+  "/",
+  zValidator("query", z.object({ hnURL: z.string().refine(isValidURL) })),
+  async (c) => {
+    const { hnURL } = c.req.valid("query")
+
+    const url = new URL(hnURL).toString()
+
+    const res = await fetch(url)
+
+    const rawHTML = await res.text()
+
+    const comments = rawHTML.match(/<div class="commtext c00">(.*?)<\/div>/g)
+
+    if (!comments) {
+      return c.text("No comments found")
+    }
+
+    const sanitizedComments = comments.map((comment) =>
+      sanitizeHtml(comment, {
+        allowedTags: [],
+        allowedAttributes: {},
+      })
+    )
+
+    return c.json({ comments: sanitizedComments })
+  }
+)
 
 export default app
